f3c76be8bd6c52ba065fb205b8c440e7e81d7599
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAv <159130832+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAv@users.noreply.github.com>
2024-11-19 13:58:39 +0000

    ajout des fichiers utility avec fonction de calcul de surface

---
+++ TP4/utility.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+float Calcul_surface(const Parcelle &surface)
+{
+    std::vector<Point2D> points;
+    points = surface.getForme().getSommets();
+    unsigned int vector_size = points.size;
+    int x_current = points[0].getX();                   
+    int y_current = points[0].getY();
+    int x_future = 0;
+    int y_future = 0;
+    float result = (-x_current*y_current)/2;                //premier point - seulement du négatif
+    for(unsigned int i = 0; i < (vector_size-1); i++)       // -1 pour éviter un overflow
+    {
+        x_future = points[i+1].getX();                      //on récupère les points futurs
+        y_future = points[i+1].getY();
+
+        result += (x_current*y_future - x_future*y_current); //formule donnée 
+
+        x_current = x_future;                               //on réassigne pour évitere de lire deux fois le même point
+        y_current = y_future;
+    }
+    result = result/2;                                      //division finale par deux - donc uniquement X.0 ou X.5 - un float est-il vraiment utile? 
+
+    return result;
+
+}
---
+++ TP4/utility.h
@@ -0,0 +1,3 @@
+#pragma once
+float Calcul_surface(const Parcelle &surface);
+

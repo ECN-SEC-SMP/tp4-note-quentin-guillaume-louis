f0ecb92591c60f6f8578f97cca3105181cdaff63
Guillaume-ctrl <ps3aknew@gmail.com>
2024-11-25 15:23:56 +0100

    Class Zu finie (en sepérant que ça soit bon)

--- TP4/Constructible.h
+++ TP4/Constructible.h
@@ -3,6 +3,7 @@
 
 #include <string.h>
 #include <iostream>
+#include "Parcelle.h"
 
 
 template<typename T>
--- TP4/ZU.h
+++ TP4/ZU.h
@@ -8,29 +8,42 @@ using std::string;
 template<typename T>
 class ZU : public Constructible<T> {
 public:
-	ZU() = default;
-	ZU(T value);
+	ZU() = delete;
+	ZU(const Parcelle& p, T surfaceConstruite = 0);
+	~ZU();
 
 	T surfaceConstructible() override;
+	void setSurfaceConstruite(const T& surfaceConstructible) const;
 
 	static string typeZone() override {
 		return string("Zone Urbaine");
 	}
 
 protected:
-	T surface_;
+	Parcelle* parcelle_;
+	T surfaceConstruite_;
 };
 
 template<typename T>
-ZU<T>::ZU(T value)
+ZU<T>::ZU(const Parcelle& p, T surfaceConstruite)
 {
+	parcelle_ = &p;
+	surfaceConstruite_ = surfaceConstruite;
+}
 
+template<typename T>
+ZU<T>::~ZU() {
+	parcelle_ = nullptr;
 }
 
 template<typename T>
 T ZU<T>::surfaceConstructible() {
-	//FINIR
+	return parcelle_->getSurface() - surfaceConstruite_;
 }
 
+template<typename T>
+void ZU<T>::setSurfaceConstruite(const T& surfaceConstructible) const {
+	surfaceConstruite_ = surfaceConstructible;
+}
 
 #endif // ZU_H

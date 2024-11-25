1b7cc5ce2ba3e9c3c007cb76159082b7b0c196e4
Frag <frag@vtd.com>
2024-11-25 16:48:50 +0100

    Removed Calcul_surface to fix errors (skill issue again)

--- TP4/utility.cpp
+++ TP4/utility.cpp
@@ -5,33 +5,6 @@
 #include <sstream>
 #include <cstdlib>
 
-float Calcul_surface(const Polygone<int>& form)
-{
-    std::vector<Point2D<int>> points;
-    points = form.getSommets();
-    unsigned int vector_size = points.size();
-    int x_current = points[0].getX();                   
-    int y_current = points[0].getY();
-    int x_future = 0;
-    int y_future = 0;
-    float result = 0.0;                //premier point - nul
-    for(unsigned int i = 0; i < (vector_size-1); i++)       // -1 pour éviter un overflow
-    {
-        x_future = points[i+1].getX();                      //on récupère les points futurs
-        y_future = points[i+1].getY();
-
-        result += (x_current*y_future - x_future*y_current); //formule donnée 
-
-        x_current = x_future;                               //on réassigne pour évitere de lire deux fois le même point
-        y_current = y_future;
-    }
-    result = result/2;                                      //division finale par deux - donc uniquement X.0 ou X.5 - un float est-il vraiment utile? 
-
-    return result;
-
-}
-
-
 std::vector<std::pair<int, int>> extractCoordinates(const std::string& input) {
     std::vector<std::pair<int, int>> coordinates;
     size_t start = 0, end;

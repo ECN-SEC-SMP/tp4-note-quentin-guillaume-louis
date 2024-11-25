79a6cc802b09a2bc51bb5066c0d7d563442afd6f
Frag <frag@vtd.com>
2024-11-25 15:43:17 +0100

    Added doxygen

--- TP4/Constructible.h
+++ TP4/Constructible.h
@@ -1,18 +1,47 @@
-#ifndef CONSTRUCTIBLE_H
-#define CONSTRUCTIBLE_H
+#pragma once
 
 #include <string.h>
 #include <iostream>
 #include "Parcelle.h"
 
-
+/**
+ * @class Constructible
+ * @brief Abstract base class for defining constructible zones.
+ *
+ * This class provides an interface for determining the surface area of
+ * a constructible zone and retrieving its type. It is meant to be
+ * derived by classes that specify concrete zone types.
+ *
+ * @tparam T The type used to represent the surface area (e.g., int, float).
+ */
 template<typename T>
-class Constructible {
+class Constructible
+{
 public:
-	Constructible() = default;
-	
-	virtual T surfaceConstructible() = 0;
-	static virtual std::string typeZone() const = 0;
-};
+    /**
+     * @brief Default constructor.
+     *
+     * Allows the creation of derived class objects.
+     */
+    Constructible() = default;
 
-#endif // CONSTRUCTIBLE_H
+    /**
+     * @brief Pure virtual method to compute the constructible surface area.
+     *
+     * Derived classes must implement this method to provide the surface
+     * area of the constructible zone.
+     *
+     * @return The surface area of the constructible zone as type T.
+     */
+    virtual T surfaceConstructible() = 0;
+
+    /**
+     * @brief Pure virtual method to retrieve the type of the zone.
+     *
+     * Derived classes must implement this method to specify the type
+     * of zone as a string.
+     *
+     * @return A string representing the type of the zone.
+     */
+    static virtual std::string typeZone() const = 0;
+};
